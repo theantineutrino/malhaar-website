0cfd7938bccdeb8faca0908acd47526459c0d3a7
shubham-sharma-2323 <sks230123@gmail.com>
2022-10-09 11:50:46 +0530

    dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,13 @@ import menu from "../images/more.png";
 import logo from "../images/logomal.png";
 import ins from "../images/insta.png";
 export default function Header() {
+  function display(){
+    let a=1;
+    if(a%2===1){
+      document.getElementById("drop").style.display = "block";
+      document.getElementById("drop").style.animationName = "dropp";
+    }
+  }
   return (
     <div className="header--container">
       <div className="header--logo">
@@ -10,7 +17,7 @@ export default function Header() {
       </div>
       <div className="header--menu">
         <div className="header--element">
-          <img src={menu} alt="" height="24px" />
+        <button style={{backgroundColor:"black"}} onClick={display}><img src={menu} alt="" height="24px" /></button>
         </div>
         <div className="header--element">
           <img src={ins} alt="" />
