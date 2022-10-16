461f87e82954ede797fe4f088282325deb6e0634
vaibhav4859 <vsachdeva4859@gmail.com>
2022-10-16 23:19:13 +0530

    Basic Structure

---
+++ vbv/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Sponsors from './components/Header/Sponsors';
+import Footer from './components/UI/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Sponsors />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
