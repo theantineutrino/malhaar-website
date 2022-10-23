712177964f17edd770d1760bd47b1f2f940c8976
Divya Sharma <divyaonline20@gmail.com>
2022-10-23 22:39:34 +0530

    footer changes

--- src/ux/Footer.js
+++ src/ux/Footer.js
@@ -5,16 +5,17 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <div className="bg-black md:flex ">
-      <Link to="/" className="m-auto w-1/3 md:w-1/4 md:h-1/4 items-center">
-        <img src={logo} alt="malhaar-logo" />
-      </Link>
-      <div className="text-white ">
+      <div className="m-auto w-1/3 md:w-1/4">
+        <Link to="/">
+          <img src={logo} alt="malhaar-logo" />
+        </Link>
+      </div>
+      <div className="text-white">
         <p className="md:w-1/2 p-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis augue
           justo, porttitorvitae felis nec, molestie sodales mi. Suspendisse eget
           aliquam ex, mollis lacinia nunc.
         </p>
-
         <div className="w-full ">
           <ul className="items-center flex flex-col gap-2 justify-center font-normal md:flex md:flex-row md:gap-5 md:justify-end p-9 text-xl md:font-semibold">
             <li>
